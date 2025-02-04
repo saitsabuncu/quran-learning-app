a78eccfe01d6d29c8a3c34c1b3f7b29aea468c18
Mustafa Sait Sabuncu <sabuncumustafasait@gmail.com>
2025-02-05 00:01:38 +0300

    Superuser oluşturuldu ve admin paneli çalıştırıldı

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    email=models.EmailField(unique=True) # Kullanıcı girişini email ile yapacağız
+
+    def __str__(self):
+        return self.username
