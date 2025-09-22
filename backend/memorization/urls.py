d81237bcfd7031d4b49dad68baffaef0df7d585e
sait <sait@example.com>
2025-09-22 05:59:32 +0300

    APIView

--- backend/memorization/urls.py
+++ backend/memorization/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import ListCreateMemorizedAyet, DeleteMemorizedAyet
+from .views import MemorizationStatsView
 
 urlpatterns = [
     path('memorized/', ListCreateMemorizedAyet.as_view(), name='memorized-list-create'),
     path('memorized/<int:ayet_id>/', DeleteMemorizedAyet.as_view(), name='memorized-delete'),
+    path('memorized/stats/', MemorizationStatsView.as_view(), name='memorization-stats'),
 ]
--- backend/memorization/views.py
+++ backend/memorization/views.py
@@ -1,9 +1,10 @@
 from rest_framework import generics, permissions
 from .models import MemorizedAyet
 from .serializers import MemorizedAyetSerializer
-from surahs.models import Ayet
+from surahs.models import Ayet, Surah
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 
 class ListCreateMemorizedAyet(generics.ListCreateAPIView):
     serializer_class = MemorizedAyetSerializer
@@ -44,3 +45,38 @@ class DeleteMemorizedAyet(generics.DestroyAPIView):
             return Response({'message': 'Silindi'}, status=status.HTTP_204_NO_CONTENT)
         except MemorizedAyet.DoesNotExist:
             return Response({'error': 'Ezber kaydı bulunamadı'}, status=status.HTTP_404_NOT_FOUND)
+
+class MemorizationStatsView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        total_ayets = Ayet.objects.count()
+        memorized_ayets = MemorizedAyet.objects.filter(user=user).count()
+
+        # Avoid division by zero
+        overall_percentage = (memorized_ayets / total_ayets * 100) if total_ayets > 0 else 0
+        surah_stats = []
+        
+        #Sıralama eklendi
+        surahs = Surah.objects.all().order_by("id")
+
+        for surah in surahs:
+            surah_total = Ayet.objects.filter(surah=surah).count()
+            surah_memorized = MemorizedAyet.objects.filter(user=user, ayet__surah=surah).count()
+            percentage = (surah_memorized / surah_total * 100) if surah_total > 0 else 0
+
+            surah_stats.append({
+                "surah_id": surah.id,
+                "surah_name": surah.name,
+                "memorized": surah_memorized,
+                "total": surah_total,
+                "percentage": round(percentage, 2)
+            })
+
+        return Response({
+            "total_memorized": memorized_ayets,
+            "total_ayets": total_ayets,
+            "overall_percentage": round(overall_percentage, 2),
+            "by_surah": surah_stats
+        })    
