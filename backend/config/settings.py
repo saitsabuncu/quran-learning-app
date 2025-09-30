81ba9eb445ce8dbfe22d3f37a6cafedc80602e74
sait <sait@example.com>
2025-09-30 05:05:58 +0300

    settings update.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -148,7 +148,7 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 # Kullanıcı için özel statik dosyalar (senin elinle koyduğun dosyalar)
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "frontend", "static"),
+    os.path.join(BASE_DIR, "backend", "static"),
 ]
 # Statik kök dizini (collectstatic için)
 STATIC_ROOT = os.path.join(BASE_DIR, "frontend", "staticfiles")
